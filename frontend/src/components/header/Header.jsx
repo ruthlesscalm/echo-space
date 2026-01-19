fe8591c180b3b358243d7b0907dbdb6ae127b63c
Ruthless Calm <ruthlesscalm@proton.me>
2026-01-19 12:49:55 +0530

    Update header design

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <header className="bg-slate-800 text-white flex justify-between px-5 py-3 items-center">
       <div>
-        <h1 className="text-3xl tracking-wide text-blue-300 font-space-grotesk">
+        <h1 className="text-3xl tracking-wide text-blue-400 font-space-grotesk font-medium">
           Echo Space
         </h1>
       </div>
@@ -22,7 +22,11 @@ const Header = () => {
               <li key={label}>
                 <a
                   href={href}
-                  className="relative text-slate-300 hover:text-white transition-colors after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-0 after:bg-blue-400 after:transition-all hover:after:w-full"
+                  className={
+                    label !== "Login"
+                      ? `relative text-slate-300 hover:text-white transition-colors after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-0 after:bg-blue-400 after:transition-all hover:after:w-full`
+                      : "bg-blue-700 font-bold px-5 py-2 rounded-md hover:bg-blue-600 transition-colors"
+                  }
                 >
                   {label}
                 </a>
