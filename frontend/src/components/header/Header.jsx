ce193e073facc40d9875a8004cdff4de585c61d2
Ruthless Calm <ruthlesscalm@proton.me>
2026-02-08 18:21:25 +0530

    Use navlink instead of link and change style for active page

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import SearchBar from "../SearchBar";
 
 const Header = () => {
   const links = [
-    { label: "Home", href: "/" },
-    { label: "About", href: "/about" },
-    { label: "Login", href: "/login" },
+    { label: "Home", href: "/", varient: "link" },
+    { label: "About", href: "/about", varient: "link" },
+    { label: "Login", href: "/login", varient: "button" },
   ];
+  function setStyle(isActive, varient) {
+    if (varient === "button") {
+      return `bg-blue-700 font-bold px-5 py-2 rounded-md hover:bg-blue-600 transition-colors`;
+    } else {
+      return `relative ${isActive ? "text-blue-400 after:w-full" : "text-slate-300 hover:text-white hover:after:w-full"} transition-colors after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-0 after:bg-blue-400 after:transition-all`;
+    }
+  }
 
   return (
     <header className="bg-slate-800 text-white flex justify-between px-5 py-3 items-center">
@@ -18,19 +25,15 @@ const Header = () => {
       <SearchBar />
       <nav>
         <ul className="flex gap-10 text-xl">
-          {links.map(({ label, href }) => {
+          {links.map(({ label, href, varient }) => {
             return (
               <li key={label}>
-                <Link
+                <NavLink
                   to={href}
-                  className={
-                    label !== "Login"
-                      ? `relative text-slate-300 hover:text-white transition-colors after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-0 after:bg-blue-400 after:transition-all hover:after:w-full`
-                      : "bg-blue-700 font-bold px-5 py-2 rounded-md hover:bg-blue-600 transition-colors"
-                  }
+                  className={({ isActive }) => setStyle(isActive, varient)}
                 >
                   {label}
-                </Link>
+                </NavLink>
               </li>
             );
           })}
