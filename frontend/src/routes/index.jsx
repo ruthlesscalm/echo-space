3e6314315270bdf38a0e724395248ffda94146d2
Ruthless Calm <ruthlesscalm@proton.me>
2026-02-08 18:27:15 +0530

    Add auth layout and make register and login its children

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,13 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import AuthLayout from "../layouts/AuthLayout";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import About from "../pages/About";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
     children: [
       {
         path: "login",
@@ -16,10 +32,6 @@ const router = createBrowserRouter([
       {
         path: "register",
         element: <Register />,
-      },
-      {
-        path: "about",
-        element: <About />,
       },
     ],
   },
