a107c8a27d0e44b48fbe3699b5a7e64b478f1660
Ruthless Calm <ruthlesscalm@proton.me>
2026-02-17 13:14:26 +0530

    Add Login form

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -3,10 +3,7 @@ import LogoText from "../components/LogoText";
 
 const AuthLayout = () => {
   return (
-    <div className="flex flex-col gap-20 min-h-screen bg-linear-to-br from-slate-900 via-slate-800 to-slate-900 text-white">
-      <header className="px-10 py-7">
-        <LogoText className="text-4xl" />
-      </header>
+    <div className="flex flex-col min-h-screen bg-linear-to-br from-slate-900 via-slate-800 to-slate-900 text-white">
       <main>
         <Outlet />
       </main>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,46 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
+  function handleInput(e) {
+    setLoginForm((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
   return (
-    <div>
+    <div className="mt-22">
       <div>
-        <h2 className="text-2xl font-bold text-white mb-6 text-center">
-          Welcome Back
-        </h2>
+        <h2 className="text-6xl text-white mb-6 text-center">Welcome Back</h2>
       </div>
-      <form></form>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={loginForm.email}
+            onChange={handleInput}
+            id="email"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={loginForm.password}
+            onChange={handleInput}
+            id="password"
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
