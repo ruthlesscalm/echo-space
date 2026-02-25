ff38a33992ce3067b654c0f2ce47b70a6e90fdf4
Ruthless Calm <ruthlesscalm@proton.me>
2026-02-25 12:37:25 +0530

    Make Login page responsive

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -53,17 +53,17 @@ const Login = () => {
 
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="text-center text-2xl/9 font-bold tracking-tight text-white">
+          <h2 className="text-center text-[clamp(1.125rem,0.625vw+0.75rem,1.875rem)] font-bold tracking-tight text-white">
             Sign in to your account
           </h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="mt-[clamp(35px,5.1vw,72px)] mx-auto w-[min(384px,90vw)] ">
           <form className="space-y-6" onSubmit={handleLoginForm}>
             <div>
               <label
                 htmlFor="email"
-                className="block text-sm/6 font-medium text-gray-100"
+                className="block text-[clamp(0.75rem,0.75rem+0.19vw,1.125rem)] font-medium text-gray-100"
               >
                 Email address
               </label>
@@ -74,9 +74,10 @@ const Login = () => {
                   name="email"
                   required
                   autoComplete="email"
+                  placeholder="Enter your email address"
                   value={loginForm.email}
                   onChange={handleInput}
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 text-[clamp(12px,0.78rem+0.21vw,1rem)] border border-slate-600 focus-within:border-slate-400 focus-within:ring focus-within:ring-slate-400"
                 />
               </div>
             </div>
@@ -85,14 +86,14 @@ const Login = () => {
               <div className="flex items-center justify-between">
                 <label
                   htmlFor="password"
-                  className="block text-sm/6 font-medium text-gray-100"
+                  className="block text-[clamp(0.75rem,0.75rem+0.19vw,1.125rem)] font-medium text-gray-100"
                 >
                   Password
                 </label>
-                <div className="text-sm">
+                <div>
                   <a
                     href="#"
-                    className="font-semibold text-indigo-400 hover:text-indigo-300"
+                    className="font-semibold text-indigo-400 hover:text-indigo-300 text-[clamp(0.75rem,0.25vw+0.625rem,0.9375rem)]"
                   >
                     Forgot password?
                   </a>
@@ -107,23 +108,24 @@ const Login = () => {
                   value={loginForm.password}
                   onChange={handleInput}
                   autoComplete="current-password"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                  placeholder="Enter your password"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 text-[clamp(12px,0.78rem+0.21vw,1rem)] border border-slate-600 focus-within:border-slate-400 focus-within:ring focus-within:ring-slate-400"
                 />
               </div>
             </div>
 
-            <div>
+            <div className="mt-8">
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className="flex w-full justify-center rounded-md bg-indigo-500 px-5 py-2 text-[clamp(0.75rem,0.25vw+0.625rem,0.9375rem)] font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
                 Sign in
               </button>
             </div>
           </form>
 
-          <p className="mt-10 text-center text-sm/6 text-gray-400">
-            Not a member?
+          <p className="flex gap-1 justify-center mt-10 text-[clamp(0.75rem,0.25vw+0.625rem,0.9375rem)] text-gray-400">
+            New to Echo Space?
             <Link
               to="/register"
               className="font-semibold text-indigo-400 hover:text-indigo-300"
